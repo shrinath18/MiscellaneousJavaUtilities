0b1609c1984c75172bea5a5bf556f3f749e2a405
Shrinath Kalamdani <shrinath.kalamdani@flipkart.com>
2015-11-01 15:56:59 +0530

    Initial Project Structure setup

---
+++ Java/Poirot/poirot-container/src/main/java/shrinath/learning/poirot/container/App.java
@@ -0,0 +1,13 @@
+package shrinath.learning.poirot.container;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
