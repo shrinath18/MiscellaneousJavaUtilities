4ebdf605236af509cd8e4e897eb878d8997a1996
Shrinath Kalamdani <shrinath.kalamdani@flipkart.com>
2015-11-25 20:33:26 +0530

    Sample producer to publish the message to Kafka

--- Java/Poirot/poirot-controller/src/main/java/shrinath/learning/poirot/controller/ProductViewController.java
+++ Java/Poirot/poirot-controller/src/main/java/shrinath/learning/poirot/controller/ProductViewController.java
@@ -1,5 +1,10 @@
 package shrinath.learning.poirot.controller;
 
+import java.util.Properties;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
 import shrinath.learning.poirot.client.models.request.ProductViewEventRequest;
 import shrinath.learning.poirot.client.models.response.ProductViewEventResponse;
 
@@ -8,6 +13,15 @@ public enum ProductViewController
     INSTANCE;
     public ProductViewEventResponse publishProductViewEvent(ProductViewEventRequest request){
     	System.out.println("Publish event to kafka!!"+request.toString());
+    	Properties properties = new Properties();
+    	properties.put("metadata.broker.list", "localhost:9092");
+    	properties.put("serializer.class", "kafka.serializer.StringEncoder");
+    	properties.put("request.required.acks", "1");
+    	
+    	Producer<Integer, String> producer = new Producer<Integer, String>(new ProducerConfig(properties));
+    	KeyedMessage<Integer, String> message = new KeyedMessage<Integer, String>("test_topic", "Hello Kafka!!!");
+    	producer.send(message);
+    	producer.close();
     	ProductViewEventResponse response = new ProductViewEventResponse();
     	response.status = "SUCCESS";
     	return response;
